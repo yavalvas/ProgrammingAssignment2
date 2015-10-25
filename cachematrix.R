fa7374c9fda7f02ee44bcc76605164c6053f38ac
Alexander <yavalvas@gmail.com>
2015-10-25 13:11:24 +0300

    Add realization of cacheSolve function + example of usage
    Realization of the cacheSolve function, which makes cache of matrix if it wasn't cached yet. Instructions are after the function.

--- cachematrix.R
+++ cachematrix.R
@@ -24,8 +24,32 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
+# cacheSolve function returns the inverse of the created matrix.
+# It checks if the inverse was computed. This function gets the cached result and don't make any computing.
+# If inverse wasn't computed yet, the function computes it ans sets the result to the cache by setter function.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverted <- x$getInverse()
+    if(!is.null(inverted)) {
+        message("cached data.")
+        return(inverted)
+    }
+    data <- x$get()
+    inverted <- solve(data)
+    x$setInverse(inverted)
+    inverted
 }
+
+# The example of usage:
+#> x = cbind(c(9.4, 2.5, 6.3), c(21/2, 3., 43.), c(54,2/4, 5.6))
+#> cacheList = makeCacheMatrix(x)
+#> cacheSolve(cacheList)
+#             [,1]        [,2]          [,3]
+#[1,] -0.001015932  0.48920356 -0.0338824048
+#[2,] -0.002345289 -0.06215773  0.0281650867
+#[3,]  0.019151394 -0.07307143  0.0004215036
+#> cacheSolve(cacheList)
+#cached data.
+#             [,1]        [,2]          [,3]
+#[1,] -0.001015932  0.48920356 -0.0338824048
+#[2,] -0.002345289 -0.06215773  0.0281650867
+#[3,]  0.019151394 -0.07307143  0.0004215036
